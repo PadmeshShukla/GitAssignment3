0ec3c92c7ef8e4c84ddfb46fae89870175e95728
Padmesh1842 <2023ucp1842@mnit.ac.in>
2024-10-14 15:10:17 +0530

    Commit dotprod.py from master

---
+++ dotprod.py
@@ -0,0 +1,11 @@
+x1 = int(input("Enter the value of x component of vector1"))
+x2 = int(input("Enter the value of y component of vector1"))
+x3 = int(input("Enter the value of z component of vector1"))
+print("\n")
+
+y1 = int(input("Enter the value of x component of vector2"))
+y2 = int(input("Enter the value of y component of vector2"))
+y3 = int(input("Enter the value of z component of vector2"))
+
+dotprod = x1 * y1 + x2 * y2 + x3 * y3
+print("dot product = " , dotprod)
